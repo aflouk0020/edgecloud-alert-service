af49dfbeafbde7413bee5478ff5308e95c0307ec
Taha Aflouk <tahaaflouk94@gmail.com>
2026-05-28 19:55:32 +0100

    SCRUM-19 configure alert service database integration

---
+++ src/main/java/com/edgecloud/alert/EdgecloudAlertServiceApplication.java
@@ -0,0 +1,13 @@
+package com.edgecloud.alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgecloudAlertServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgecloudAlertServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edgecloud/alert/EdgecloudAlertServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgecloud.alert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgecloudAlertServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
